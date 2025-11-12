ce3d47d68f17e211da44e0ddecc83433cc33b7e9
peg <ameba23@systemli.org>
2025-11-12 10:34:38 +0100

    Update CI to include dependency for azure

--- src/attestation/azure.rs
+++ src/attestation/azure.rs
@@ -1,4 +1,6 @@
+use az_tdx_vtpm::{hcl, imds, report, tdx, vtpm};
 use tokio_rustls::rustls::pki_types::CertificateDer;
+// use openssl::pkey::{PKey, Public};
 
 use crate::attestation::{compute_report_input, AttestationError, AttestationType, QuoteGenerator};
 
@@ -18,6 +20,23 @@ impl QuoteGenerator for MaaQuoteGenerator {
     ) -> Result<Vec<u8>, AttestationError> {
         let quote_input = compute_report_input(cert_chain, exporter)?;
 
+        let td_report = report::get_report().unwrap();
+        let td_quote_bytes = imds::get_td_quote(&td_report).unwrap();
+
+        let bytes = vtpm::get_report().unwrap();
+        let hcl_report = hcl::HclReport::new(bytes).unwrap();
+        let var_data_hash = hcl_report.var_data_sha256();
+        let ak_pub = hcl_report.ak_pub().unwrap();
+
+        let td_report: tdx::TdReport = hcl_report.try_into().unwrap();
+        assert!(var_data_hash == td_report.report_mac.reportdata[..32]);
+
+        // let nonce = "a nonce".as_bytes();
+        //
+        // let tpm_quote = vtpm::get_quote(nonce).unwrap();
+        // let der = ak_pub.key.try_to_der().unwrap();
+        // let pub_key = PKey::public_key_from_der(&der).unwrap();
+        // tpm_quote.verify(&pub_key, nonce).unwrap();
         todo!()
     }
 }
